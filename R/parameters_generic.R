898b830dff8649a36cf64e133b10455ddb54718c
Ben Phillips <ben.l.phillips@curtin.edu.au>
2025-08-15 11:47:41 +0800

    add new density dependent decay function to parameters

--- R/parameters_generic.R
+++ R/parameters_generic.R
@@ -2,6 +2,7 @@
 # Parameters
 fecundity <- 5                              # Number of offspring per day per female mosquito
 prob_survival <- 0.7
+dd_rate <- 0.0016
 patches <- 7                               # Number of patches
 n_per_patch <- c(10000,0,0,0,0,0,0)    # Initial number of individuals per patch
 beta <- 100                           # the adult male population size at which the daily probability of mating is 0.5.
